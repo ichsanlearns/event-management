ed3e20453221ca989560c3247220f0f192f543f9
Muhammad Ichsanudin <michsanudin04@gmail.com>
2026-01-23 19:47:06 +0700

    feat: implement authentication flow

---
+++ api/src/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { prisma } from "../lib/prisma.lib.js";
+
+import { Role } from "../generated/prisma/enums.js";
+
+export async function create(
+  name: string,
+  email: string,
+  password: string,
+  role: Role,
+  referralCode: string,
+) {
+  return await prisma.user.create({
+    data: { name, email, password, role, referral_code: referralCode },
+  });
+}
+
+export async function getByEmail(email: string, password: string) {
+  return await prisma.user.findUnique({
+    where: { email },
+    select: { password: true },
+  });
+}
