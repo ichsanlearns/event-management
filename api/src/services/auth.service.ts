65f4ce3d53c421b9144249d57d5117a59b5e96d9
FajarWnugra <fajarwn474@gmail.com>
2026-01-26 10:26:59 +0700

    hashing password done

--- api/src/services/auth.service.ts
+++ api/src/services/auth.service.ts
@@ -1,28 +1,26 @@
 import { prisma } from "../lib/prisma.lib.js";
-
 import { Role } from "../generated/prisma/enums.js";
 
-export async function create(
-  name: string,
-  email: string,
-  password: string,
-  role: Role,
-  referralCode: string,
-) {
-  return await prisma.user.create({
-    data: { name, email, password, role, referral_code: referralCode },
+export async function createUser(name: string, email: string, hashedPassword: string, role: Role, referralCode: string) {
+  return prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      role,
+      referral_code: referralCode,
+    },
   });
 }
 
-export async function getByEmail(email: string, password: string) {
-  return await prisma.user.findUnique({
+export async function findByEmail(email: string) {
+  return prisma.user.findUnique({
     where: { email },
-    select: { password: true },
   });
 }
 
-export async function getByReferral(referralCode: string) {
-  return await prisma.user.findUnique({
+export async function findByReferral(referralCode: string) {
+  return prisma.user.findUnique({
     where: { referral_code: referralCode },
   });
 }
