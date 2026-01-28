1c515a464b37c1ff083db7f633b818727339708c
Muhammad Ichsanudin <michsanudin04@gmail.com>
2026-01-28 22:17:24 +0700

    feat: add interactive event search dropdown shell

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,6 +1,64 @@
+import { useEffect, useRef, useState } from "react";
 import Card from "../components/Card";
+import type { TEvent } from "../types/event.type";
+import { Link } from "react-router";
 
 function Home() {
+  const [events, setEvents] = useState<TEvent[] | null>(null);
+  const [query, setQuery] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/events`);
+        const data = await response.json();
+        setEvents(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchEvents();
+  }, []);
+
+  // close when clicking outside
+  useEffect(() => {
+    function handleClickOutside(e: MouseEvent) {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(e.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  useEffect(() => {
+    if (!query) return;
+
+    const id = setTimeout(() => {
+      async function fetchSearchResults() {
+        try {
+          const response = await fetch(
+            `${import.meta.env.VITE_API_URL}/events`,
+          );
+          const data = await response.json();
+          setEvents(data.data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      fetchSearchResults();
+      console.log("test");
+    }, 1500);
+    return () => clearTimeout(id);
+  }, [query]);
+
   return (
     <main className="w-screen h-screen">
       <div className="relative w-full h-[700px] lg:h-[800px] flex items-center justify-center ">
@@ -10,7 +68,7 @@ function Home() {
             className="w-full h-full object-cover"
             src="https://lh3.googleusercontent.com/aida-public/AB6AXuBUvDnYowdfgxkqfFk3zOhWziFg5ZAs6Go8Aqvi8QzdQj0aIbN13F_-VTfUqLrgTCdbHxFQcMYyZNVyHlqvp_O0_okGZnMXnDRkoLmpHeadjAu4jdUWVK2kaY9-n09PMvMICBm69TLn6zYwZKUqhjeFu6PVZoIhz309JwDtuvY-272zJdOEWYWR-UFBOHo33xE51v6NwGMgxaPVzswvUJAlUGbdKLPu-WHdk6UmvhT6X1IASQ9XzWVkzN3uUG11vBmED3r9NB4DPWM"
           />
-          <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/40 to-black/80"></div>
+          <div className="absolute inset-0 bg-linear-to-b from-black/60 via-black/40 to-black/80"></div>
         </div>
         <div className="relative z-10 w-full max-w-[1280px] mx-auto px-4 sm:px-6 lg:px-8 text-center flex flex-col items-center pt-20">
           <span className="inline-block py-1 px-3 rounded-full bg-white/10 border border-white/20 text-white text-xs font-semibold uppercase tracking-wider mb-6 backdrop-blur-md">
@@ -32,17 +90,39 @@ function Home() {
         <div className="absolute bottom-0 w-full z-20 translate-y-1/2 px-4">
           <div className="max-w-[1000px] mx-auto bg-white dark:bg-[#1E1B2E] rounded-3xl shadow-2xl border border-gray-100 dark:border-gray-700 p-3 md:p-4">
             <form className="flex flex-col lg:flex-row gap-3">
-              <div className="relative flex-grow group">
+              <div ref={containerRef} className="relative grow group ">
                 <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
                   <span className="material-symbols-outlined text-gray-400 group-focus-within:text-primary transition-colors">
                     search
                   </span>
                 </div>
                 <input
-                  className="block w-full pl-12 pr-4 py-4 border-none rounded-2xl bg-gray-50 dark:bg-gray-800/50 text-gray-900 dark:text-white placeholder-gray-400 focus:ring-0 focus:bg-white dark:focus:bg-gray-800 transition-all text-lg font-medium"
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  onFocus={() => setIsOpen(true)}
+                  className="block w-full pl-12 pr-4 py-4 border-none rounded-t-2xl bg-gray-50 dark:bg-gray-800/50 text-gray-900 dark:text-white placeholder-gray-400 focus:ring-0 focus:bg-white dark:focus:bg-gray-800 transition-all text-lg font-medium"
                   placeholder="Search events, artists, or venues..."
                   type="text"
                 />
+                {isOpen && query && (
+                  <>
+                    {/* dropdown */}
+                    <div className="absolute top-full left-0 right-0 bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700  rounded-b-xl shadow-xl z-50">
+                      {/* scroll area */}
+                      <div className="max-h-72 overflow-y-auto overscroll-contain">
+                        {events?.map((event) => (
+                          <Link
+                            to={`/event/${event.id}`}
+                            key={event.id}
+                            className="block px-4 py-3 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800"
+                          >
+                            {event.name}
+                          </Link>
+                        ))}
+                      </div>
+                    </div>
+                  </>
+                )}
               </div>
               <div className="flex flex-col sm:flex-row gap-3">
                 <div className="flex gap-2 overflow-x-auto no-scrollbar py-1 sm:py-0">
@@ -70,7 +150,7 @@ function Home() {
                   </div>
                 </div>
                 <button
-                  className="flex-shrink-0 bg-primary hover:bg-primary-dark text-white font-bold py-3.5 px-8 rounded-2xl transition-all shadow-lg shadow-indigo-500/20 active:scale-95 flex items-center justify-center gap-2 text-lg"
+                  className="shrink-0 bg-primary hover:bg-primary-dark text-white font-bold py-3.5 px-8 rounded-2xl transition-all shadow-lg shadow-indigo-500/20 active:scale-95 flex items-center justify-center gap-2 text-lg"
                   type="button"
                 >
                   Search
