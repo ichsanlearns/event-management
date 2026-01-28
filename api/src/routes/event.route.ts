b63122dce2a0b6bc160f59de0dae79e5f794df7a
FajarWnugra <fajarwn474@gmail.com>
2026-01-28 11:27:41 +0700

    add role middleware

--- api/src/routes/event.route.ts
+++ api/src/routes/event.route.ts
@@ -2,9 +2,19 @@ import express from "express";
 
 import * as eventController from "../controllers/event.controller.js";
 
+import { authMiddleware } from "../middleware/auth.middleware.js";
+import { organizerOnly } from "../middleware/role.middleware.js";
+
 const router = express.Router();
 
 router.get("/", eventController.getAll);
 router.get("/:id", eventController.getById);
+
+router.post("/", authMiddleware, organizerOnly, (req, res) => {
+  res.json({
+    message: "Event berhasil dibuat (dummy)",
+    user: req.user,
+  });
+});
 
 export default router;
