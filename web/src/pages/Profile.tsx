a03accc39e2267c045eb836f8808bbc4b81783e2
FajarWnugra <fajarwn474@gmail.com>
2026-02-03 11:00:07 +0700

    edit profile.tsx

--- web/src/pages/Profile.tsx
+++ web/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { User, Mail, Shield, Gift, LogOut, Edit } from "lucide-react";
 import { useNavigate } from "react-router";
 import api from "../lib/api";
