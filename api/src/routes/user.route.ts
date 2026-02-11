55b9bc0f5e98d6eb6647d04dc96a44b25002c8fb
FajarWnugra <fajarwn474@gmail.com>
2026-02-11 09:40:13 +0700

    edit image.service

--- api/src/routes/user.route.ts
+++ api/src/routes/user.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
-import { getUserPointAndCoupon, updateProfile, getMe, changePassword, updateProfileImage } from "../controllers/user.controller.js";
-import { uploadProfile } from "../middleware/upload.middleware.js";
+import { getUserPointAndCoupon, updateProfile, getMe, changePassword, uploadProfileImage } from "../controllers/user.controller.js";
+import { uploadCloud } from "../middleware/upload.middleware.js";
 
 const router = Router();
 
@@ -9,6 +9,6 @@ router.get("/me", authMiddleware, getMe);
 router.put("/me", authMiddleware, updateProfile);
 router.put("/change-password", authMiddleware, changePassword);
 router.get("/rewards", authMiddleware, getUserPointAndCoupon);
-router.put("/profile/image", authMiddleware, uploadProfile.single("image"), updateProfileImage);
+router.put("/profile/image", authMiddleware, uploadCloud.single("image"), uploadProfileImage);
 
 export default router;
--- api/src/services/image.service.ts
+++ api/src/services/image.service.ts
@@ -1,6 +1,7 @@
 import { uploadSingleUtil } from "../utils/upload.util.js";
 import { prisma } from "../lib/prisma.lib.js";
 import { AppError } from "../utils/app-error.util.js";
+import { cloudinary } from "../lib/cloudinary.lib.js";
 
 export async function uploadSingleService(file: Express.Multer.File) {
   if (!file) {
@@ -15,3 +16,21 @@ export async function uploadSingleService(file: Express.Multer.File) {
 
   return imageResult.url;
 }
+
+export const uploadToCloudinary = (buffer: Buffer, folder = "profile"): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream(
+        {
+          folder,
+          resource_type: "image",
+          transformation: [{ width: 300, height: 300, crop: "fill", gravity: "face" }],
+        },
+        (error, result) => {
+          if (error || !result) return reject(error);
+          resolve(result.secure_url);
+        },
+      )
+      .end(buffer);
+  });
+};
