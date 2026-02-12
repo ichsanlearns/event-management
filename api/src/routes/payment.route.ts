d06d4077d0e4b55483b42c0d56be88e5cc1f2803
Muhammad Ichsanudin <michsanudin04@gmail.com>
2026-02-12 12:14:35 +0700

    feat(payment): integrate payment creation api to fe with image proof

--- api/src/routes/payment.route.ts
+++ api/src/routes/payment.route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { createPayment, getPayments } from "../controllers/payment.controller.js";
-import { uploadCloud } from "../middleware/upload.middleware.js";
+import {
+  createPayment,
+  getPayments,
+} from "../controllers/payment.controller.js";
+import { uploadLocal } from "../middleware/upload.middleware.js";
 
 const router = Router();
 
 router.get("/", getPayments);
-router.post("/", uploadCloud.single("singleImage"), createPayment);
+router.post("/", uploadLocal.single("singleImage"), createPayment);
 
 export default router;
