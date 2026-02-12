79c30ff55e2def62512a74d3171eb9403e8a02f5
FajarWnugra <fajarwn474@gmail.com>
2026-02-12 23:31:15 +0700

    edit feature update event

--- api/src/controllers/event.controller.ts
+++ api/src/controllers/event.controller.ts
@@ -1,6 +1,6 @@
 import { type Request, type Response } from "express";
 
-import { create, getAll, getById } from "../services/event.service.js";
+import { create, getAll, getById, getByOrganizerId, remove, updateById } from "../services/event.service.js";
 
 import { redis } from "../lib/redis.lib.js";
 import { createEventSchema } from "../validators/event.validator.js";
@@ -66,4 +66,77 @@ export async function getEventById(req: Request, res: Response) {
   res.status(200).json({ message: "Event berhasil diambil", data: event });
 }
 
-export async function getEventByOrganizerId(req: Request, res: Response) {}
+export async function getEventByOrganizerId(req: Request, res: Response) {
+  const organizerIdParam = req.params.organizerId;
+  const organizerId = Array.isArray(organizerIdParam) ? organizerIdParam[0] : organizerIdParam;
+
+  if (!organizerId) {
+    return res.status(400).json({
+      message: "Organizer ID is required",
+    });
+  }
+
+  const cacheKey = `events:organizer:${organizerId}`;
+
+  const cached = await redis.get(cacheKey);
+  if (cached) {
+    const data = JSON.parse(cached);
+    return res.status(200).json({
+      message: "Events fetched from cache",
+      data,
+      length: data.length,
+    });
+  }
+
+  const events = await getByOrganizerId(organizerId);
+
+  await redis.set(cacheKey, JSON.stringify(events), "EX", 60 * 5);
+
+  res.status(200).json({
+    message: "Events fetched by organizer",
+    data: events,
+    length: events.length,
+  });
+}
+
+export async function updateEvent(req: Request, res: Response) {
+  const idParam = req.params.id;
+  const id = Array.isArray(idParam) ? idParam[0] : idParam;
+
+  if (!id) {
+    return res.status(400).json({ message: "ID event diperlukan" });
+  }
+
+  const validatedData = createEventSchema.partial().parse(req.body);
+
+  const result = await updateById(id, validatedData);
+
+  const keys = await redis.keys("events:organizer:*");
+  if (keys.length > 0) {
+    await redis.del(keys);
+  }
+
+  res.status(200).json({
+    message: "Event updated",
+    data: result,
+  });
+}
+
+export async function deleteEvent(req: Request, res: Response) {
+  const idParam = req.params.id;
+  const id = Array.isArray(idParam) ? idParam[0] : idParam;
+
+  if (!id) {
+    res.status(400).json({ message: "ID event diperlukan" });
+    return;
+  }
+
+  await remove(id);
+
+  const keys = await redis.keys("events:*");
+  if (keys.length > 0) {
+    await redis.del(keys);
+  }
+
+  res.status(200).json({ message: "Event berhasil dihapus" });
+}
